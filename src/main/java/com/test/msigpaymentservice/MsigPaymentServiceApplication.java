6f1c9879ee91bbb7e27f5be2557f89d477c4f67f
dhymasws <dimaswahyuara@gmail.com>
2026-03-26 10:25:47 +0700

    initial commit

---
+++ src/main/java/com/test/msigpaymentservice/MsigPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.test.msigpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsigPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsigPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/msigpaymentservice/MsigPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.msigpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsigPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
